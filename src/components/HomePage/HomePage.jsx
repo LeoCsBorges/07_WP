899417265a53b436a0f415d1b856043ef644c36d
Léo Borges <leo_borgess@live.com>
2025-07-11 11:53:04 -0300

    Adding initial project estructure

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import styles from "./HomePage.module.css";
+
+export const HomePage = () => {
+  return (
+    <>
+      <h2 className={styles.teste}>MINHA HOME PAGE</h2>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  );
+};
+
+export default HomePage;

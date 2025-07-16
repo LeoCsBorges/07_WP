2c367e2930340c411044250dad5b358478250ef8
Léo Borges <leo_borgess@live.com>
2025-07-15 23:50:18 -0300

    Adding layout responsivity 1.0

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -2,6 +2,16 @@ import * as Dialog from "@radix-ui/react-dialog";
 import { Link } from "react-router-dom";
 import styles from "./MobileMenu.module.css";
 
+// Importando os ícones que vamos usar
+import {
+  FaHome,
+  FaTools,
+  FaImage,
+  FaGlobe,
+  FaPlay,
+  FaEnvelope,
+} from "react-icons/fa";
+
 export const MobileMenu = () => {
   return (
     <Dialog.Root>
@@ -11,12 +21,30 @@ export const MobileMenu = () => {
         <Dialog.Content className={styles.content}>
           <Dialog.Close className={styles.close}>&times;</Dialog.Close>
           <nav className={styles.menuNav}>
-            <Link to="/">Principal</Link>
-            <Link to="/servicos">Serviços</Link>
-            <Link to="/banners">Banners</Link>
-            <Link to="/web">Web</Link>
-            <Link to="/motion">Motion</Link>
-            <Link to="/contato">Contato</Link>
+            <Link to="/">
+              <FaHome />
+              Principal
+            </Link>
+            <Link to="/servicos">
+              <FaTools />
+              Serviços
+            </Link>
+            <Link to="/banners">
+              <FaImage />
+              Banners
+            </Link>
+            <Link to="/web">
+              <FaGlobe />
+              Web
+            </Link>
+            <Link to="/motion">
+              <FaPlay />
+              Motion
+            </Link>
+            <Link to="/contato">
+              <FaEnvelope />
+              Contato
+            </Link>
           </nav>
         </Dialog.Content>
       </Dialog.Portal>
